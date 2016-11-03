d33a4894eb2ef1f4d33e8e7fcac94bdc8f912beb
mpyw <ryosuke_i_628@yahoo.co.jp>
2016-11-03 15:54:37 +0900

    Second commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# Microsoft Excel で開ける CSV 関連のフォーマットまとめ
+
+## プラットフォーム別早見表
+
+2010年より古いものは考慮しません。
+
+### 凡例
+
+```
+◎: 一発で開けた
+◯: 開こうとするとウィザードが出現するが，デフォルトの設定で開けた
+△: 開こうとするとウィザードが出現し，区切り文字を変更すれば開けた
+✕: 文字化けしたり表示が崩れたりして開けなかった
+```
+
+### Windows
+
+#### Office 2010
+
+```
+```
+
+#### Office 2013
+
+```
+```
+
+#### Office 2016
+
+```
+```
+
+### Mac
+
+#### Office for Mac 2011
+
+```
+```
+
+
+#### Office 2016 for Mac
+
+```
+Shift_JIS.CSV.csv: ◎
+Shift_JIS.CSV.tsv: △
+Shift_JIS.TSV.csv: ✕
+Shift_JIS.TSV.tsv: ◯
+UTF-16_LE.CSV.csv: △
+UTF-16_LE.CSV.tsv: △
+UTF-16_LE.TSV.csv: ◯
+UTF-16_LE.TSV.tsv: ◯
+```
+
+## Contribution
+
+不明である場所は空けてあります。空欄埋めにご協力ください。  
+`questionnaire_form.txt`の形式で Pull Request または Issue でご報告ください。
+
---
+++ generator.php
@@ -0,0 +1,51 @@
+<?php
+
+$rows = [
+
+];
+
+$charsets = [
+    'Shift_JIS'  => ['cp932', ""],
+    'UTF-16_LE' => ['utf-16le', "\xEF\xBB\xBF"],
+];
+$delimiters = [
+    'CSV' => ',',
+    'TSV' => "\t",
+];
+$extensions = ['csv', 'tsv'];
+
+$form = fopen(__DIR__ . '/questionnaire_form.txt', 'wb');
+fwrite($form, <<<EOD
+<< アンケートフォーム >>
+
+【環境】
+
+OS: 
+Officeバージョン: 
+
+
+【動作結果】
+
+以下の記号でお答えください
+
+◎: 一発で開けた
+◯: 開こうとするとウィザードが出現するが，デフォルトの設定で開けた
+△: 開こうとするとウィザードが出現し，区切り文字を変更すれば開けた
+✕: 文字化けしたり表示が崩れたりして開けなかった
+
+
+EOD
+);
+
+foreach ($charsets as $charset => list($to, $bom)) {
+    foreach ($delimiters as $format => $delimiter) {
+        foreach ($extensions as $extension) {
+            fwrite($form, "$charset.$format.$extension: \n");
+            $fp = fopen(__DIR__ . "/files/$charset.$format.$extension", 'wb');
+            stream_filter_prepend($fp, "convert.iconv.utf-8/$to");
+            fwrite($fp, $bom);
+            fputcsv($fp, ['佐倉綾音', '保登心愛'], $delimiter);
+            fputcsv($fp, ['水瀬いのり', '香風智乃'], $delimiter);
+        }
+    }
+}
