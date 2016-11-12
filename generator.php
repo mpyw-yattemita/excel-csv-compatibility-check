c110263a1c591d867624f38add8d33a0272412cc
mpyw <ryosuke_i_628@yahoo.co.jp>
2016-11-13 00:43:28 +0900

    大幅に更新
    - 結論の取り下げ
    - UTF-8およびUTF-8Nのチェックを追加
    - 文字コードだけでなく改行コードのチェックも追加

--- generator.php
+++ generator.php
@@ -24,12 +24,17 @@ Officeバージョン:
 
 【動作結果】
 
-以下の記号でお答えください
+以下の書式でお答えください
 
 ◎: 一発で開けた
 ◯: 開こうとするとウィザードが出現するが，デフォルトの設定で開けた
 △: 開こうとするとウィザードが出現し，区切り文字を変更すれば開けた
 ✕: 文字化けしたり表示が崩れたりして開けなかった
+
+文字コード.実際の中身のフォーマット.拡張子: 記号 [正しく表示された改行コード]
+
+例   Shift_JIS.CSV.csv: ◎ [CRLF][LF]
+
 
 
 EOD
@@ -42,8 +47,10 @@ foreach ($charsets as $charset => list($to, $bom)) {
             $fp = fopen(__DIR__ . "/files/$charset.$format.$extension", 'wb');
             stream_filter_prepend($fp, "convert.iconv.utf-8/$to");
             fwrite($fp, $bom);
-            fputcsv($fp, ['佐倉綾音', '保登心愛'], $delimiter);
-            fputcsv($fp, ['水瀬いのり', '香風智乃'], $delimiter);
+            fputcsv($fp, ['改行コード', 'キャラ名 (氏名間で改行)', '声優名 (氏名間で改行)']);
+            fputcsv($fp, ["CRLF", "保登\r\n心愛", "佐倉\r\n綾音"], $delimiter);
+            fputcsv($fp, ["LF", "香風\n智乃", "水瀬\nいのり"], $delimiter);
+            fputcsv($fp, ["CR", "桐間\r紗路", "内田\r真礼"], $delimiter);
         }
     }
 }
